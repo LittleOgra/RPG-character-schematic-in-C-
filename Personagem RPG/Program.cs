c166fd0a033f487443d717c1e93e81bfeaaf0d2f
Pichau <Pichau@DESKTOP-JEU5D0O>
2023-04-23 12:36:28 -0300

    Adicionar arquivos de projeto.

---
+++ Personagem RPG/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personagem_RPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Personagem guerreiro = new Personagem();
+            Console.WriteLine("Qual o nome do seu personagem?");
+            guerreiro.name = Console.ReadLine();
+
+            guerreiro.hp = 10;
+            guerreiro.defense = 2;
+            guerreiro.strength = 2;
+
+
+            Console.WriteLine("Nome: " + guerreiro.name);
+            Console.WriteLine("Hp: " + guerreiro.hp);
+            Console.WriteLine("Força de ataque: " + guerreiro.strength);
+            Console.WriteLine("Defesa: " + guerreiro.defense);
+            Console.ReadLine();
+
+            Console.WriteLine("Um inimigo atacou!");
+            guerreiro.Attacked();
+          
+            Console.ReadLine() ;
+
+        }
+    }
+}
---
+++ Personagem RPG/personagem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personagem_RPG
+{
+    public class Personagem
+    {
+        public string name;
+        public string classe;
+        public int hp;
+        public int strength;
+        public int defense;
+
+       
+
+        public void Attacked() 
+        {
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            
+           do 
+            {
+                this.hp = hp + defense - diceRoll;
+            } while (diceRoll > this.hp + defense);
+
+
+            if (diceRoll <= defense)
+            {
+                Console.WriteLine("O inimigo tirou " + diceRoll + " no dado. Ele errou!");
+                Console.ReadLine();
+            }
+            else Console.WriteLine("O inimigo tirou " + diceRoll + ". O ataque acertou, sua vida restante é: " + this.hp);
+
+        }
+
+        
+    }
+}
